13ebec2395dd9961bb775c76b2d78b74a4460a5d
shane <psh40963@naver.com>
2022-05-03 22:35:53 +0900

    REST: Put and ObjectMapper

---
+++ mvc/src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
@@ -0,0 +1,51 @@
+package com.example.mvc.controller.put
+
+import com.example.mvc.model.http.Result
+import com.example.mvc.model.http.UserRequest
+import com.example.mvc.model.http.UserResponse
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api")
+class PutApiController {
+
+    @PutMapping("/put-mapping")
+    fun putMapping(): String {
+        return "put-mapping"
+    }
+
+    @RequestMapping(method = [RequestMethod.PUT], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping PutMethod"
+    }
+
+    @PutMapping(path = ["/put-mapping/object"])
+    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
+
+        // 1. create userResponse
+        return UserResponse().apply {
+            // 2. create Result
+            this.result = Result().apply {
+                this.resultCode = "OK"
+                this.resultMessage = "SUCCESS"
+            }
+        }.apply {
+            // 3. create description
+            this.description = "detailed description"
+        }.apply {
+            // 3. create user mutable list
+            val userList = mutableListOf<UserRequest>()
+            userList.add(userRequest)
+            userList.add(UserRequest().apply {
+                this.name = "shane"
+                this.age = 10
+                this.email = "shane@java.com"
+                this.address = "queenstown"
+                this.phoneNumber = "123-4567"
+            })
+            this.userRequest = userList
+        }
+
+    }
+
+}
